ecb8c20f05808d71bfed19567ade9d423aea775b
Kevin <xiaohuidexinge@163.com>
2014-02-18 16:52:58 +0800

    tiny test.

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -3,5 +3,5 @@
 	<a href="/index.php/api">API</a>
 	<a href="/index.php/help">HELP US</a>
 	<a href="http://t.cn/zQS2J6s" target="_blank">Chrome 插件</a>
-	<a href="/index.php/info" target="_blank">个人无聊222</a>
+	<a href="/index.php/info" target="_blank">个人无聊</a>
 </div>
