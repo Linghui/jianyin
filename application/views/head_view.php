334b40c1f46679e86d378bcf922437fabe4a0f32
Kevin <xiaohuidexinge@163.com>
2014-02-18 22:10:03 +0800

    tiny.

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -3,5 +3,6 @@
 	<a href="/index.php/api">API</a>
 	<a href="/index.php/help">HELP US</a>
 	<a href="http://t.cn/zQS2J6s" target="_blank">Chrome 插件</a>
-	<a href="/index.php/info">个人无聊</a>
+	&nbsp;&nbsp;
+	<a href="/index.php/info">个人简介</a>
 </div>
