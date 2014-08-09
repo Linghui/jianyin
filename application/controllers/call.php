e7f9f6c7726fa758083323caada05e0ba9c20bad
Kevin <xiaohuidexinge@163.com>
2014-08-09 17:50:16 +0800

    add call api.

---
+++ application/controllers/call.php
@@ -0,0 +1,17 @@
+<?php
+
+class Call extends MY_Controller {
+
+
+	public function index()
+	{
+		$key = $this -> input -> get('k');
+		$p = $this -> input -> get('p');
+		if(!$key){
+			echo "error";
+		}
+		$this -> call_model ->save($key, $p);
+		
+		echo "ok";
+	}
+}
