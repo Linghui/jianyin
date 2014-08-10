dd2d6a4d1cae50d76633411eacdad892d73e96d9
Kevin <xiaohuidexinge@163.com>
2014-08-10 18:26:06 +0800

    tiny.

--- application/views/bang_view.php
+++ application/views/bang_view.php
@@ -6,17 +6,21 @@
 
 	</head>
 	<body>
-		<?php
-		$index = 0;
+		<h1>状元榜</h1>
+		<hr />
+		<div align="center">
+			<?php
+			$index = 1;
 
-		foreach ($todo_list as $item) {
+			foreach ($todo_list as $item) {
 
-			$pos = strpos($item -> spend_time, '.');
-			$timestr = substr($item -> spend_time, 0, $pos + 2);
+				$pos = strpos($item -> spend_time, '.');
+				$timestr = substr($item -> spend_time, 0, $pos + 2);
 
-			echo "$index. " . $item -> click_times . "次 耗时" . $timestr . "秒<br/>";
-			$index++;
-		}
-		?>
+				echo "<h2>$index. " . $item -> click_times . "次 耗时" . $timestr . "秒<br/></h2>";
+				$index++;
+			}
+			?>
+		</div>
 	</body>
 </html>
