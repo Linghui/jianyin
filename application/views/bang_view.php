747fb21db653fcf40012ac366ff62011ade2f6b5
Kevin <xiaohuidexinge@163.com>
2014-08-10 18:21:18 +0800

    tiny.

--- application/views/bang_view.php
+++ application/views/bang_view.php
@@ -10,7 +10,11 @@
 		$index = 0;
 
 		foreach ($todo_list as $item) {
-			echo "$index. " . $item -> click_times . "次 耗时" . $item -> spend_time . "秒<br/>";
+
+			$pos = strpos($item -> spend_time, '.');
+			$timestr = substr($item -> spend_time, 0, $pos + 1);
+
+			echo "$index. " . $item -> click_times . "次 耗时" . $timestr . "秒<br/>";
 			$index++;
 		}
 		?>
