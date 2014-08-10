b91531778895f268e47a0e281499bceebc8c9276
Kevin <xiaohuidexinge@163.com>
2014-08-10 18:43:08 +0800

    tiny.

--- application/views/bang_view.php
+++ application/views/bang_view.php
@@ -19,15 +19,15 @@
 				$pos = strpos($item -> spend_time, '.');
 				$timestr = substr($item -> spend_time, 0, $pos + 2);
 
-				echo "<h1>$index. " . $item -> click_times . "次 耗时" . $timestr . "秒</h1>";
+				echo "<h1>$index. " . $item -> click_times . "次 耗时" . $timestr . "秒";
 				if( $index <= 3){
-					echo " 华安一样的智商<br/>";
+					echo " -- 华安一样的智商<br/></h1>";
 				} else if ($index <= 10 ) {
-					echo " 秋香一样的智商<br/>";
+					echo " -- 秋香一样的智商<br/></h1>";
 				} else if ($index <= 15 ) {
-					echo " 华太师一样的智商<br/>";
+					echo " -- 华太师一样的智商<br/></h1>";
 				} else if ($index <= 30 ) {
-					echo "比华文华武略高那么一点点<br/>";
+					echo " -- 比华文华武略高那么一点点<br/></h1>";
 				}
 				$index++;
 			}
