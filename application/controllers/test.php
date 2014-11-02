ded9895306fa351b2b2ac96c923deec8b7c3446b
李英辉 <xiaohuidexinge@163.com>
2014-11-02 17:10:57 +0800

    no message

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -21,7 +21,7 @@ class Test extends CI_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index() {
-		echo phpinfo();
+		echo json_encode($_SERVER);
 
 	}
 
