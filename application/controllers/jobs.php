ccd02d168dad07f6667faaa3c9b11ec330b48641
李英辉 <xiaohuidexinge@163.com>
2014-11-02 13:03:33 +0800

    test

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -2,9 +2,19 @@
 
 class Jobs extends CI_Controller {
 
+	public function index() {
+		$url = "http://ehire.51job.com/MainLogin.aspx";
+		$ch = curl_init($url);
 
-	public function index()
-	{
-		echo phpinfo();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		echo $response;
 	}
+
 }
