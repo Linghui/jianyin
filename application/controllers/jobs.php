51ea3dd284640c92bd450382808642adf374d2c3
李英辉 <xiaohuidexinge@163.com>
2014-11-02 15:26:00 +0800

    1

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -13,7 +13,7 @@ class Jobs extends CI_Controller {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_VERBOSE, 1);
 		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+		// curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
 
 		$response = curl_exec($ch);
 		// $httpCode = curl_getinfo($ch, CURLINFO_HE);
@@ -53,36 +53,37 @@ class Jobs extends CI_Controller {
 		}
 		// echo json_encode($this -> get_request_headers());
 		// echo "found : " . $access_key;
+		echo $response;
 
-		$url = "https://ehirelogin.51job.com/Member/UserLogin.aspx";
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-		$POST_DATA = array(ctmName => urlencode("中广互联"), userName => "zghl863", password => "cnjobs2014", checkCode => "", oldAccessKey => $access_key, langtype => "Lang=&Flag=1", isRememberMe => "false", sc => "07e6a30d0c0dd9c6", ec => "687dc60d8ca8492f880d64999dc9218c");
-
-		$postfields = http_build_query($POST_DATA);
-		curl_setopt($ch, CURLOPT_PORT, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $this -> get_request_headers());
-
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_VERBOSE, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		$cookie_file = tempnam("tmp", "cookie.txt");
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
-		$rrr = curl_setopt($ch, CURLOPT_CAINFO, "/usr/share/doc/libssl-doc/demos/cms/cacert.pem");
-		if (!$rrr) {
-			echo curl_error($ch);
-		} else {
-			$response = curl_exec($ch);
-			echo $response;
-		}
-
-		curl_close($ch);
+		// $url = "https://ehirelogin.51job.com/Member/UserLogin.aspx";
+		// $ch = curl_init();
+// 
+		// curl_setopt($ch, CURLOPT_URL, $url);
+		// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+// 
+		// $POST_DATA = array(ctmName => urlencode("中广互联"), userName => "zghl863", password => "cnjobs2014", checkCode => "", oldAccessKey => $access_key, langtype => "Lang=&Flag=1", isRememberMe => "false", sc => "07e6a30d0c0dd9c6", ec => "687dc60d8ca8492f880d64999dc9218c");
+// 
+		// $postfields = http_build_query($POST_DATA);
+		// curl_setopt($ch, CURLOPT_PORT, 1);
+		// curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+// 
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, $this -> get_request_headers());
+// 
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		// curl_setopt($ch, CURLOPT_HEADER, 1);
+		// // $cookie_file = tempnam("tmp", "cookie.txt");
+		// // curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+		// $rrr = curl_setopt($ch, CURLOPT_CAINFO, "/usr/share/doc/libssl-doc/demos/cms/cacert.pem");
+		// if (!$rrr) {
+			// echo curl_error($ch);
+		// } else {
+			// $response = curl_exec($ch);
+			// echo $response;
+		// }
+// 
+		// curl_close($ch);
 
 	}
 
