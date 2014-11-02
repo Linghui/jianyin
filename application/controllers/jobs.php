812e73d35765be862a4416b2fa42d7575d3bca20
李英辉 <xiaohuidexinge@163.com>
2014-11-02 14:15:10 +0800

    1

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -42,7 +42,7 @@ class Jobs extends CI_Controller {
 						$pieces = explode("=", $one);
 						if (count($pieces) == 2) {
 							$k = $pieces[0];
-							echo "1111 = =$k " . count($k) . "<br/>";
+							echo "1111 = =$k " . strlen($k) . "<br/>";
 							$v = $pieces[1];
 
 							if (strcmp($k, "AccessKey") == 0) {
