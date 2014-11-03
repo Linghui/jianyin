2085b2bfc85195bd08974d6e93de9ceb8a8c14f0
李英辉 <xiaohuidexinge@163.com>
2014-11-03 22:05:24 +0800

    no message

--- application/controllers/mongo_test.php
+++ application/controllers/mongo_test.php
@@ -2,9 +2,18 @@
 
 class Mongo_test extends CI_Controller {
 
+	function getUser($username) {
+		$this -> load -> library('mongo_db');
 
-	public function index()
-	{
-		$this->load->view('template_html');
+		$user = $this -> mongo_db -> get_where('users', array('username' => $username));
+		return $user;
 	}
+
+	function createUser($username, $password) {
+		$this -> load -> library('mongo_db');
+		$user = array('username' => $username, 'password' => $password);
+
+		$this -> mongo_db -> insert('users', $user);
+	}
+
 }
