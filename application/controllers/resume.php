2d75d6b1204fdba02ceec9572a37495214f6844a
李英辉 <xiaohuidexinge@163.com>
2014-11-04 16:13:57 +0800

    no message

--- application/controllers/resume.php
+++ application/controllers/resume.php
@@ -4,10 +4,23 @@ class Resume extends CI_Controller {
 
 	public function add() {
 		$name = $this -> getp("name");
-		echo $name;
 
 		$sex = $this -> getp("sex");
-		echo $sex;
+
+		$age = $this -> getp("age");
+
+		$phone = $this -> getp("phone");
+
+		$card_id = $this -> getp("card_id");
+
+		$email = $this -> getp("email");
+
+		$location = $this -> getp("location");
+
+		$education = $this -> getp("education");
+
+		$job_history = $this -> getp("job_history");
+
 	}
 
 	private function getp($p_name) {
