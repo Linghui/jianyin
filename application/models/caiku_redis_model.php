6721e611a07f9c2483e7e0e19196c50cff4c1197
李英辉 <xiaohuidexinge@163.com>
2014-11-04 17:16:23 +0800

    1

--- application/models/caiku_redis_model.php
+++ application/models/caiku_redis_model.php
@@ -5,18 +5,18 @@ class Caiku_redis_model extends CI_Model {
 	public function save($name, $sex, $card_id, $phone, $email) {
 
 		if (!$this -> redis -> hget("resume_id")) {
+			echo "new";
 			$this -> redis -> hmset("resume_id", "test");
 		}
+		echo "over";
 
-		if (!$this -> redis -> hget("resume_id", $name)) {
-			$this -> redis -> hmset("resume_id", array($name => "test"));
-		}
-
-		if (!$this -> redis -> hget("resume_id", $name, $sex)) {
-			$this -> redis -> hmset("resume_id", array($name => array($sex => "test")));
-		}
-		
-		
+		// if (!$this -> redis -> hget("resume_id", $name)) {
+		// $this -> redis -> hmset("resume_id", array($name => "test"));
+		// }
+		//
+		// if (!$this -> redis -> hget("resume_id", $name, $sex)) {
+		// $this -> redis -> hmset("resume_id", array($name => array($sex => "test")));
+		// }
 
 		return;
 	}
