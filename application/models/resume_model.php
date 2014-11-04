a3291cfc1c853446ee160018131d7a2d86785c3f
李英辉 <xiaohuidexinge@163.com>
2014-11-05 01:11:41 +0800

    no message

--- application/models/resume_model.php
+++ application/models/resume_model.php
@@ -4,9 +4,22 @@ class Resume_model extends CI_Model {
 
 	public function add($name, $sex, $birth, $card_id, $phone, $email) {
 		$this -> load -> library('mongo_db');
+
+		$resume = array('name' => $name, 'sex' => $sex, 'birth' => $birth, 'card_id' => $card_id, 'phone' => $phone, 'email' => $email);
+
+		// check if there is a very same one.
+		$data = $user = $this -> mongo_db -> get_where('resume', $resume);
+		if ($data) {
+			return 0;
+		}
+
 		$resume_id = uniqid("resume_id_");
-		$resume = array('resume_id' => $resume_id, 'name' => $name, 'sex' => $sex, 'birth' => $birth, 'card_id' => $card_id, 'phone' => $phone, 'email' => $email);
+
+		$resume['resume_id'] = $resume_id;
+
 		$this -> mongo_db -> insert('resume', $resume);
+
+		return $resume_id;
 	}
 
 }
