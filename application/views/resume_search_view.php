282401f59c4fcbfc2c7e6a2fd1bf99b016c73632
李英辉 <xiaohuidexinge@163.com>
2014-11-05 21:50:50 +0800

    no message

--- application/views/resume_search_view.php
+++ application/views/resume_search_view.php
@@ -1,10 +1,6 @@
 <head>
 	<meta charset="utf-8">
 	<title>才库搜索</title>
-	<script type="text/javascript" src="/js/jquery.min.js"></script>
-	<script type="text/javascript" src="/javascript/semantic.min.js"></script>
-	<script type="text/javascript" src="/js/resume.js"></script>
-	<link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
 </head>
 <body>
 	<div class="ui input">
@@ -14,4 +10,9 @@
 		<i class="search icon"></i>
 		搜索
 	</div>
+
+	<script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/javascript/semantic.min.js"></script>
+	<script type="text/javascript" src="/js/resume.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
 </body>
