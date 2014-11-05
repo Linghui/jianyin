615444e696fe819a538a9da9e48135fe608ff9d8
李英辉 <xiaohuidexinge@163.com>
2014-11-05 21:41:36 +0800

    no message

--- application/views/resume_search_view.php
+++ application/views/resume_search_view.php
@@ -5,6 +5,11 @@
 	<link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
 </head>
 <body>
-	<input type="text" value=""/>
-	<input type="button" value="搜索"/>
+	<div class="ui input">
+		<input type="text" placeholder="搜索...">
+	</div>
+	<div class="ui right labeled icon button">
+		<i class="search icon"></i>
+		搜索
+	</div>
 </body>
