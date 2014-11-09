bc8319dfbb336cb6ffba97b2d3b2ffe9682eeb11
李英辉 <xiaohuidexinge@163.com>
2014-11-09 22:26:32 +0800

    no message

--- js/resume.js
+++ js/resume.js
@@ -872,9 +872,6 @@ function initArea() {
 
 	var index = 0;
 	for (var key in ja) {
-		if(index >= 10){
-			break;
-		}
 		console.log("key " + key);
 		var cityCode = key;
 		var cityName = ja[key];
