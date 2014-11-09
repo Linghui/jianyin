ae5fe3ad133f45423960c3523ab579b1e07edbbd
李英辉 <xiaohuidexinge@163.com>
2014-11-09 20:04:11 +0800

    no message

--- cgi/test.pl
+++ cgi/test.pl
@@ -3,5 +3,15 @@
 use strcit;
 use CGI;
 
-print "hello world";
+print "Content-type:text/html\n\n";
+print <<EndOfHTML;
+<html><head><title>Perl Environment Variables</title></head>
+<body>
+<h1>Perl Environment Variables</h1>
+EndOfHTML
 
+foreach $key (sort(keys %ENV)) {
+    print "$key = $ENV{$key}<br>\n";
+}
+
+print "</body></html>";
