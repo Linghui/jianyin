8fae862a934d6e2fd9eec402dda183d476f09220
李英辉 <xiaohuidexinge@163.com>
2014-11-09 22:16:41 +0800

    no message

--- js/resume.js
+++ js/resume.js
@@ -867,7 +867,11 @@ function initArea() {
 	// html += '</div>';
 	// });
 
+	var index = 0;
 	for (var key in ja) {
+		if(index >= 10){
+			break;
+		}
 		console.log("key " + key);
 		var cityCode = key;
 		var cityName = ja[key];
@@ -880,6 +884,7 @@ function initArea() {
 		html += '">';
 		html += cityName;
 		html += '</div>';
+		index++;
 	}
 	console.log("html " + html);
 	$("#city").html(html);
