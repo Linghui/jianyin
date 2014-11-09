747c1894a0351d067614b44255b2423660f3c797
李英辉 <xiaohuidexinge@163.com>
2014-11-09 22:08:26 +0800

    no message

--- js/resume.js
+++ js/resume.js
@@ -1,74 +1,3 @@
-$().ready(initArea);
-
-$("#search").click(function() {
-	search();
-});
-
-function search() {
-
-	var word = $("#search_wrod").val();
-
-	$.ajax({
-		url : "resume_search/search?w=" + word,
-		dataType : "json"
-	}).done(function(data) {
-		console.log("done");
-		if (data.status > 0) {
-			show_resume_list(data.data);
-		} else if (data.status == 0) {
-			show_error("no data found");
-		} else {
-			show_error("need search word");
-		}
-
-	}).fail(function(data) {
-		console.log("failed " + data);
-	});
-}
-
-function show_resume_list(resume_list) {
-	var html = "";
-	for (var index = 0; index < resume_list.length; index++) {
-		html += '<div class="item">';
-		html += '<div class="content">';
-		html += '<div class="header">姓名:' + resume_list[index].name + '</div>';
-		html += '性别:' + resume_list[index].sex + " ";
-		html += '生日:' + resume_list[index].birth + " ";
-		html += '住址:' + resume_list[index].location + " ";
-		html += '</div>';
-		html += '</div>';
-		html += '</div>';
-	}
-	console.log(html);
-	$("#resume_list").html(html);
-}
-
-function show_error(msg) {
-	$("#resume_list").html(msg);
-}
-
-function initArea() {
-	console.log("initArea");
-	var html = "";
-	for(var key in ja){
-		var cityCode = ja;
-		var cityName = ja;
-		if(isMainCity(cityCode)){
-			continue;
-		}
-		
-		html += '<div class="item" data-value="';
-		html += cityCode;
-		html += '">';
-		html += cityName;
-		html += '</div>';
-	}
-	$("#city").html(html);
-}
-
-function isMainCity(){
-	return true;
-}
 
 
 var ja = [];
@@ -874,4 +803,77 @@ ja['350000'] = '台湾';
 ja['360000'] = '国外';
 
 var maincity = [[['010000', '020000', '040000', '030200', '070200', '180200', '200200', '080200', '090200', '060000', '050000', '070300', '080300', '120300', '230200', '230300', '030800', '110200', '120200', '150200', '170200', '190200', '220200', '240200', '250200', '070400', '070500']]];
+
+$().ready(initArea);
+
+$("#search").click(function() {
+	search();
+});
+
+function search() {
+
+	var word = $("#search_wrod").val();
+
+	$.ajax({
+		url : "resume_search/search?w=" + word,
+		dataType : "json"
+	}).done(function(data) {
+		console.log("done");
+		if (data.status > 0) {
+			show_resume_list(data.data);
+		} else if (data.status == 0) {
+			show_error("no data found");
+		} else {
+			show_error("need search word");
+		}
+
+	}).fail(function(data) {
+		console.log("failed " + data);
+	});
+}
+
+function show_resume_list(resume_list) {
+	var html = "";
+	for (var index = 0; index < resume_list.length; index++) {
+		html += '<div class="item">';
+		html += '<div class="content">';
+		html += '<div class="header">姓名:' + resume_list[index].name + '</div>';
+		html += '性别:' + resume_list[index].sex + " ";
+		html += '生日:' + resume_list[index].birth + " ";
+		html += '住址:' + resume_list[index].location + " ";
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+	}
+	console.log(html);
+	$("#resume_list").html(html);
+}
+
+function show_error(msg) {
+	$("#resume_list").html(msg);
+}
+
+function initArea() {
+	console.log("initArea");
+	var html = "";
+	for(var key in ja){
+		var cityCode = ja;
+		var cityName = ja;
+		if(isMainCity(cityCode)){
+			continue;
+		}
+		
+		html += '<div class="item" data-value="';
+		html += cityCode;
+		html += '">';
+		html += cityName;
+		html += '</div>';
+	}
+	console.log(html);
+	$("#city").html(html);
+}
+
+function isMainCity(){
+	return true;
+}
 
