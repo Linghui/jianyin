326636d8b8a5c61c7bd9d818821074214089c3ae
李英辉 <xiaohuidexinge@163.com>
2014-11-09 22:18:52 +0800

    no message

--- application/views/resume_search_view.php
+++ application/views/resume_search_view.php
@@ -23,6 +23,6 @@
 
 	<script type="text/javascript" src="/js/jquery.min.js"></script>
 	<script type="text/javascript" src="/javascript/semantic.min.js"></script>
-	<script type="text/javascript" src="/js/resume.js"></script>
 	<link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
+	<script type="text/javascript" src="/js/resume.js"></script>
 </body>
