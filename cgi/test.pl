51de02fd35e53d0bf991a9b80cc6f0da48498da6
李英辉 <xiaohuidexinge@163.com>
2014-11-18 23:43:47 +0800

    no message

--- cgi/test.pl
+++ cgi/test.pl
@@ -2,12 +2,17 @@
 
 use strict;
 use CGI;
+use LWP::UserAgent;
 
 print "Content-type:text/html\n\n";
 
 my $newua = LWP::UserAgent->new;
 my $response = $newua->get('http://www.jian-yin.com/cgi/51.pl');
 
+print $response->status_line;
+print "\n";
+
 my $content = $response->decoded_content;
 
 print $content;
+print "\n";
