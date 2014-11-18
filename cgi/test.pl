6a7674de698b244429aaa0f1c73ecfe744c7c247
李英辉 <xiaohuidexinge@163.com>
2014-11-18 23:41:06 +0800

    no message

--- cgi/test.pl
+++ cgi/test.pl
@@ -5,4 +5,9 @@ use CGI;
 
 print "Content-type:text/html\n\n";
 
-print "hello world";
+my $newua = LWP::UserAgent->new;
+my $response = $newua->get('http://www.jian-yin.com/cgi/51.pl');
+
+my $content = $response->decoded_content;
+
+print $content;
