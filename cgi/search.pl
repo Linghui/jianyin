87bdbb3036d30fcc091e8014262f5e94249665c5
李英辉 <xiaohuidexinge@163.com>
2014-11-18 22:08:21 +0800

    no message

--- cgi/search.pl
+++ cgi/search.pl
@@ -81,10 +81,10 @@ if( $content =~ /MainLogin/){
     system("rm -rf $cookie_base/51_cookies.txt");
     # do log in
     my $newua = LWP::UserAgent->new;
-    my $response = $newua->get('http://www.jian-yin.com/cgi/51.pl');
+    my $response = `curl http://www.jian-yin.com/cgi/51.pl`;
     
 #    print $response->decoded_content;
-    if( $response->decoded_content =~ /强制下线/){
+    if( $response =~ /强制下线/){
         $resp{$key_code} =$ERROR;
         $resp{$key_message} ='账户已经登陆，请登录51job企业账号强制下线后，再次尝试搜索';
         
