419c22b01605e5a938ed76876b1e99713ad5851e
李英辉 <xiaohuidexinge@163.com>
2014-11-25 20:19:40 +0800

    no message

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,7 @@
 $().ready(init);
 
 $("#search").click(function() {
-	search_51();
+	// search_51();
 	search_self();
 });
 
@@ -166,7 +166,7 @@ function show_resume_list_new(resume_list) {
 }
 
 function show_resume_list_self(resume_list) {
-	console.log("show_resume_list_self");
+	console.log("show_resume_list_self	");
 	var html = "";
 	for (var index = 0; index < resume_list.length; index++) {
 
