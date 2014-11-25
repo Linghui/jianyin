934b6c12bdc80be77ce35e159d7fad08ae29421e
李英辉 <xiaohuidexinge@163.com>
2014-11-25 22:24:34 +0800

    no message

--- application/views/resume_search_view.php
+++ application/views/resume_search_view.php
@@ -94,6 +94,44 @@
 	<div class="ui selection list" id="self_resume_list"></div>
 	<div class="ui selection list" id="51_resume_list" style="display:none"></div>
 
+	<div id="context1">
+		<div class="ui pointing secondary menu">
+			<a class="item" data-tab="first">First</a>
+			<a class="item active" data-tab="second">Second</a>
+		</div>
+		<div class="ui tab segment" data-tab="first">
+			<div class="ui top attached tabular menu">
+				<a class="active item" data-tab="first/a">1A</a>
+				<a class="item" data-tab="first/b">1B</a>
+				<a class="item" data-tab="first/c">1C</a>
+			</div>
+			<div class="ui bottom attached active tab segment" data-tab="first/a">
+				1A
+			</div>
+			<div class="ui bottom attached tab segment" data-tab="first/b">
+				1B
+			</div>
+			<div class="ui bottom attached tab segment" data-tab="first/c">
+				1C
+			</div>
+		</div>
+		<div class="ui tab segment active" data-tab="second">
+			<div class="ui top attached tabular menu">
+				<a class="item" data-tab="second/a">2A</a>
+				<a class="item" data-tab="second/b">2B</a>
+				<a class="item active" data-tab="second/c">2C</a>
+			</div>
+			<div class="ui bottom attached tab segment" data-tab="second/a">
+				2A
+			</div>
+			<div class="ui bottom attached tab segment" data-tab="second/b">
+				2B
+			</div>
+			<div class="ui bottom attached tab segment active" data-tab="second/c">
+				2C
+			</div>
+		</div>
+	</div>
 	<script type="text/javascript" src="/js/jquery.min.js"></script>
 	<script type="text/javascript" src="/javascript/semantic.js"></script>
 	<link rel="stylesheet" type="text/css" href="/css/semantic.min.css">
