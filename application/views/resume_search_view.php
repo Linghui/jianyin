ecaa6c419278bc1f5883ac26dd31f06af92cb56c
李英辉 <xiaohuidexinge@163.com>
2014-11-25 23:11:33 +0800

    no message

--- application/views/resume_search_view.php
+++ application/views/resume_search_view.php
@@ -90,115 +90,18 @@
 		<a class="active item" onclick="watch_self()" id="self_title"> 才库 (0) </a>
 		<a class="item" onclick="watch_51()" id="51_title"> 51 (0)</a>
 	</div>
-	<div id="context1">
-		<div class="ui pointing secondary menu">
-			<a class="item" data-tab="first">First</a>
-			<a class="item active" data-tab="second">Second</a>
-			<a class="item" data-tab="third">Third</a>
-		</div>
-		<div class="ui tab segment" data-tab="first">
-			<div class="ui top attached tabular menu">
-				<a class="active item" data-tab="first/a">1A</a>
-				<a class="item" data-tab="first/b">1B</a>
-				<a class="item" data-tab="first/c">1C</a>
-			</div>
-			<div class="ui bottom attached active tab segment" data-tab="first/a">
-				1A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="first/b">
-				1B
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="first/c">
-				1C
-			</div>
-		</div>
-		<div class="ui tab segment active" data-tab="second">
-			<div class="ui top attached tabular menu">
-				<a class="item" data-tab="second/a">2A</a>
-				<a class="item" data-tab="second/b">2B</a>
-				<a class="item active" data-tab="second/c">2C</a>
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="second/a">
-				2A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="second/b">
-				2B
-			</div>
-			<div class="ui bottom attached tab segment active" data-tab="second/c">
-				2C
-			</div>
-		</div>
-		<div class="ui tab segment" data-tab="third">
-			<div class="ui top attached tabular menu">
-				<a class="item" data-tab="third/a">3A</a>
-				<a class="item" data-tab="third/b">3B</a>
-				<a class="item" data-tab="third/c">3C</a>
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="third/a">
-				3A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="third/b">
-				3B
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="third/c">
-				3C
-			</div>
-		</div>
-	</div>
 	<div id="context2">
 		<div class="ui pointing secondary menu">
 			<a class="item" data-tab="fourth">Fourth</a>
 			<a class="item active" data-tab="fifth">Fifth</a>
-			<a class="item" data-tab="sixth">Sixth</a>
 		</div>
 		<div class="ui tab segment" data-tab="fourth">
-			<div class="ui top attached tabular menu">
-				<a class="active item" data-tab="fourth/a">4A</a>
-				<a class="item" data-tab="fourth/b">4B</a>
-				<a class="item" data-tab="fourth/c">4C</a>
-			</div>
-			<div class="ui bottom attached active tab segment" data-tab="fourth/a">
-				4A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="fourth/b">
-				4B
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="fourth/c">
-				4C
-			</div>
+11
 		</div>
 		<div class="ui tab segment active" data-tab="fifth">
-			<div class="ui top attached tabular menu">
-				<a class="item" data-tab="fifth/a">5A</a>
-				<a class="item" data-tab="fifth/b">5B</a>
-				<a class="item active" data-tab="fifth/c">5C</a>
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="fifth/a">
-				5A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="fifth/b">
-				5B
-			</div>
-			<div class="ui bottom attached tab segment active" data-tab="fifth/c">
-				5C
-			</div>
+22
 		</div>
-		<div class="ui tab segment" data-tab="sixth">
-			<div class="ui top attached tabular menu">
-				<a class="item" data-tab="sixth/a">6A</a>
-				<a class="item" data-tab="sixth/b">6B</a>
-				<a class="item" data-tab="sixth/c">6C</a>
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="sixth/a">
-				6A
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="sixth/b">
-				6B
-			</div>
-			<div class="ui bottom attached tab segment" data-tab="sixth/c">
-				6C
-			</div>
-		</div>
+
 	</div>
 	<div class="ui selection list" id="self_resume_list"></div>
 	<div class="ui selection list" id="51_resume_list" style="display:none"></div>
