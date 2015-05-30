b15d6193a35d96c54238f7443c98607b5536671b
李英辉 <xiaohuidexinge@163.com>
2015-05-30 14:03:43 +0800

    add 塔防demo

--- application/views/info_view.php
+++ application/views/info_view.php
@@ -46,6 +46,8 @@
 			<h3>&bull;<a href='http://www.jian-yin.com/ck/launcher/release.html' >建议用微信玩儿</a></h3>
 			<h2>5.flappy bird unity 复刻</h2>
 			<h3>&bull;<a href='/img/flappybird.apk' >本地下载</a></h3>
+			<h2>6.unity 塔防demo</h2>
+			<h3>&bull;<a href='/img/EnchanterDefence.apk' >本地下载</a></h3>
 			<br/>
 			<h1 id="title">个人宠物</h1>
 			<img src='/img/when_its_little_2.jpg' width="30%" height="30%"/>
