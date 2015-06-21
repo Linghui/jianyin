ed7e7598db8bc23dd5bfe02caf6f1fc9eac3b68d
李英辉 <xiaohuidexinge@163.com>
2015-06-21 23:36:01 +0800

    add blog link

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -7,6 +7,7 @@
 	<a href="/">HOME</a>
 	<a href="/index.php/api">API</a>
 	<a href="/index.php/help">HELP US</a>
+	<a href="http://wp.jian-yin.com">Blog</a>
 	<!-- <a href="https://chrome.google.com/webstore/detail/%E7%AB%9E%E6%8A%80%E8%BF%9E%E8%BF%9E%E7%9C%8B/ecnhkgaandclbpelapofmglaecpdpami/related?hl=zh-CN&gl=ID" target="_blank">Chrome 插件</a> -->
 	&nbsp;&nbsp;
 	<a href="/index.php/info">个人简介/游戏</a>
