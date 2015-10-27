f43d79d9a3bb1d80eb2c4e012de51c8218c3e310
李英辉 <xiaohuidexinge@163.com>
2015-10-27 22:13:16 +0800

    no message

--- alipay/alipayapi.php
+++ alipay/alipayapi.php
@@ -30,7 +30,7 @@ require_once("lib/alipay_submit.class.php");
         $payment_type = "1";
         //必填，不能修改
         //服务器异步通知页面路径
-        //$notify_url = "http://watao-test.jian-yin.com/index.php/webservices/pay_h5_order";
+        $notify_url = "http://watao-test.jian-yin.com/index.php/webservices/pay_h5_order";
         //需http://格式的完整路径，不能加?id=123这类自定义参数
         //页面跳转同步通知页面路径
         $return_url = "http://www.watao.me/done.html";
