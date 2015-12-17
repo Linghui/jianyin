60c74f04fd9b69366f4836b0bc68056d1ca5209d
李英辉 <xiaohuidexinge@163.com>
2015-12-18 01:59:06 +0800

    no message

--- cocos/res/resource.js
+++
@@ -1,8 +0,0 @@
-var res = {
-    icon1: "img/icon1.png",
-    icon2: "img/icon2.png",
-    icon3: "img/icon3.png",
-    icon4: "img/icon4.png",
-    logob: "img/logob.png",
-    logos: "img/logos.png",
-};
