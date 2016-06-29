422309524aad3e2e60a3f33a6d6a3071d563f8a6
李英辉 <xiaohuidexinge@163.com>
2016-06-29 20:48:04 +0800

    no message

--- application/controllers/s.php
+++ application/controllers/s.php
@@ -8,8 +8,7 @@ class S extends CI_Controller {
         $this->load->model('short_url_model');
         $short_url = $this->short_url_model->get_by_short_url_id($short_id);
         if($short_url){
-            echo "ok " . $short_url->long_url;
-            header("Location: " + $short_url->long_url);
+            header("Location: " . $short_url->long_url);
         } else {
             echo "Do not found this short url!";
         }
